7852ae075ae4c2708880867e484fa6e71491bf7b
AlessandroZappatore <alessandrozappatore03@gmail.com>
2025-07-20 11:10:51 +0200

    Implement reusable timer component with customizable duration and notification; add styles for timer end animation

---
+++ sim-suite/src/main/java/it/uniupo/simnova/views/common/utils/ReusableTimer.java
@@ -0,0 +1,93 @@
+package it.uniupo.simnova.views.common.utils;
+
+import com.flowingcode.vaadin.addons.simpletimer.SimpleTimer;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+@CssImport("./themes/sim.suite/views/timer-styles.css")
+public class ReusableTimer extends Composite<Div> {
+    private final SimpleTimer timer;
+    private final Button toggleButton;
+    private final String titleText;
+    private final Notification.Position notificationPosition;
+
+    public ReusableTimer(String title, int durationInMinutes, Notification.Position notificationPosition) {
+        this.titleText = title;
+        this.notificationPosition = notificationPosition;
+
+        int totalSeconds = durationInMinutes * 60;
+
+        Div root = getContent();
+        root.addClassName("card-style");
+
+        timer = new SimpleTimer();
+
+        timer.setStartTime(totalSeconds);
+
+        timer.setMinutes(true);
+
+        timer.setFractions(false);
+        timer.getStyle().set("font-size", "var(--lumo-font-size-xxl)").set("font-weight", "600");
+
+        toggleButton = new Button(VaadinIcon.PLAY.create());
+        toggleButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        toggleButton.setTooltipText("Fai partire il timer");
+
+        Button restartButton = new Button(VaadinIcon.REFRESH.create());
+        restartButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        restartButton.setTooltipText("Riavvia il timer");
+
+        addListeners(restartButton);
+
+        Span titleSpan = new Span(this.titleText);
+        titleSpan.addClassNames(LumoUtility.FontSize.SMALL, LumoUtility.TextColor.SECONDARY);
+
+        HorizontalLayout buttonsLayout = new HorizontalLayout(toggleButton, restartButton);
+        VerticalLayout contentWrapper = new VerticalLayout(titleSpan, timer, buttonsLayout);
+        contentWrapper.setAlignItems(FlexComponent.Alignment.CENTER);
+        contentWrapper.setPadding(false);
+        contentWrapper.setSpacing(false);
+
+        root.add(contentWrapper);
+    }
+
+    private void addListeners(Button restartButton) {
+        toggleButton.addClickListener(e -> {
+            if (timer.isRunning()) {
+                timer.pause();
+                toggleButton.setIcon(VaadinIcon.PLAY.create());
+                toggleButton.setTooltipText("Riprendi");
+            } else {
+                timer.start();
+                toggleButton.setIcon(VaadinIcon.PAUSE.create());
+                toggleButton.setTooltipText("Metti in pausa");
+            }
+        });
+
+        restartButton.addClickListener(e -> {
+            getContent().removeClassName("timer-ended-blinking");
+            timer.reset();
+            toggleButton.setIcon(VaadinIcon.PLAY.create());
+            toggleButton.setTooltipText("Fai partire il timer");
+        });
+
+        timer.addTimerEndEvent(e -> {
+            String message = String.format("Il timer '%s' è terminato.", this.titleText);
+            Notification.show(message, 3000, this.notificationPosition)
+                    .addThemeVariants(NotificationVariant.LUMO_PRIMARY);
+
+            getContent().addClassName("timer-ended-blinking");
+        });
+    }
+}
--- sim-suite/src/main/java/it/uniupo/simnova/views/execution/ExecutionView.java
+++ sim-suite/src/main/java/it/uniupo/simnova/views/execution/ExecutionView.java
@@ -1,23 +1,17 @@
 package it.uniupo.simnova.views.execution;
 
-import com.flowingcode.vaadin.addons.simpletimer.SimpleTimer;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.NotificationVariant;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import it.uniupo.simnova.service.storage.FileStorageService;
 import it.uniupo.simnova.views.common.components.AppHeader;
+import it.uniupo.simnova.views.common.utils.ReusableTimer;
 import it.uniupo.simnova.views.common.utils.StyleApp;
 
 @PageTitle("Execution")
@@ -26,68 +20,19 @@ import it.uniupo.simnova.views.common.utils.StyleApp;
 public class ExecutionView extends Composite<VerticalLayout> {
 
     public ExecutionView(FileStorageService fileStorageService) {
-
         VerticalLayout mainLayout = StyleApp.getMainLayout(getContent());
         AppHeader header = new AppHeader(fileStorageService);
         Button backButton = StyleApp.getBackButton();
         backButton.addClickListener(event -> getUI().ifPresent(ui -> ui.navigate("")));
 
-        SimpleTimer timer = new SimpleTimer();
-        timer.setStartTime(60);
-        timer.setFractions(false);
-        timer.getStyle().set("font-size", "var(--lumo-font-size-xxl)").set("font-weight", "600");
-
-        Button toggleButton = new Button(VaadinIcon.PLAY.create());
-        toggleButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        toggleButton.setTooltipText("Fai partire il timer");
-
-        Button restartButton = new Button(VaadinIcon.REFRESH.create());
-        restartButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        restartButton.setTooltipText("Riavvia il timer");
-
-        Div timerContainer = new Div();
-        timerContainer.addClassName("card-style");
-
-        toggleButton.addClickListener(e -> {
-            if (timer.isRunning()) {
-                timer.pause();
-                toggleButton.setIcon(VaadinIcon.PLAY.create());
-                toggleButton.setTooltipText("Riprendi");
-            } else {
-                timer.start();
-                toggleButton.setIcon(VaadinIcon.PAUSE.create());
-                toggleButton.setTooltipText("Metti in pausa");
-            }
-        });
-
-        restartButton.addClickListener(e -> {
-            timerContainer.getStyle().remove("background");
-
-            timer.reset();
-            toggleButton.setIcon(VaadinIcon.PLAY.create());
-            toggleButton.setTooltipText("Fai partire il timer");
-        });
-
-        timer.addTimerEndEvent(e -> {
-            Notification.show("Il timer generale è terminato.", 3000, Notification.Position.MIDDLE)
-                    .addThemeVariants(NotificationVariant.LUMO_PRIMARY);
-
-            timerContainer.getStyle().set("background", "var(--lumo-error-color-50pct)");
-        });
-
-        Span title = new Span("Timer Generale");
-        title.addClassNames(LumoUtility.FontSize.SMALL, LumoUtility.TextColor.SECONDARY);
-
-        HorizontalLayout buttonsLayout = new HorizontalLayout(toggleButton, restartButton);
-        VerticalLayout contentWrapper = new VerticalLayout(title, timer, buttonsLayout);
-        contentWrapper.setAlignItems(FlexComponent.Alignment.CENTER);
-        contentWrapper.setPadding(false);
-        contentWrapper.setSpacing(false);
-
-        timerContainer.add(contentWrapper);
+        ReusableTimer generalTimer = new ReusableTimer(
+                "Timer Generale",
+                20,
+                Notification.Position.MIDDLE
+        );
 
         HorizontalLayout customHeader = StyleApp.getCustomHeader(backButton, header);
-        customHeader.add(timerContainer);
+        customHeader.add(generalTimer);
         customHeader.expand(header);
 
         VerticalLayout contentLayout = StyleApp.getContentLayout();
